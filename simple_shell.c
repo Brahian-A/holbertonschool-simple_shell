94f773845d1b8b6a5263d90d3cfc7139530041b9
root <10114@holbertonstudents.com>
2024-12-19 00:47:54 +0000

    creacion de la main.h donde se encuentran las biblotecas, creacion de simple_shell la cual mediante un bucle crea la shell de manera muy basica

---
+++ simple_shell.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	char *args[] = {NULL};
+
+	while (69)
+	{
+		printf("$ ");
+
+
+		if (getline(&line, &len, stdin) == -1)
+		{
+			if (feof(stdin))
+			{
+				printf("\nEOF detectado. Saliendo...\n");
+				break;
+			}
+			else
+			{
+                	perror("Error");
+                	break;
+			}
+		}
+
+
+	line[strcspn(line, "\n")] = '\0';
+
+	if (strcmp(line, "exit") == 0)
+	{
+		printf("Saliendo del programa...\n");
+		break;
+	}
+
+
+
+	if (*line == '\0')
+	continue;
+
+
+	printf("Comando ingresado: %s\n", line);
+
+
+	}
+
+	free(line);
+	return 0;
+}
