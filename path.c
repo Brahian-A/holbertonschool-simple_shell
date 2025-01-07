46079c2fe296a2f33cfa5a24b366e8c66fd534e6
Juandicode <juandiegomhs@gmail.com>
2025-01-07 02:51:01 +0000

    coments

--- path.c
+++ path.c
@@ -28,46 +28,46 @@ char *obtener_path(char *envp[])
  *Return: Ruta completa del comando si se encuentra, NULL si no se encuentra
  */
 
-char *buscar_path(char *comando, char *envp[])
+char *buscar_path(char *comando, char *envp[]) /* comando es el nombre del comado que estamos buscando, y envp es el arreglo de variables de entorno */
 {
-	char *comando_ruta, *direct, *path_copy = NULL, *path = obtener_path(envp);
-	struct stat buffer;
+	char *comando_ruta, *direct, *path_copy = NULL, *path = obtener_path(envp); /* y luego se declaran las variables comando_ruta para almacenar la ruta completa del comando , direct, para almacenar los directorios individuales, path copy, que es una copia del valor de path para evitar modificar el original, y luego tenemos la variable path, que es igual a la funcion obtner path  */
+	struct stat buffer; /* una variable de tipo struct que guarda informacion del archivo */
 
-	if (comando[0] == '/')
+	if (comando[0] == '/') /*verifica si el comando dado ya tiene la ruta absoluta(si tiene la barra es que tiene la ruta completa ) */
 	{
-		if (stat(comando, &buffer) == 0 && (buffer.st_mode & S_IXUSR))
+		if (stat(comando, &buffer) == 0 && (buffer.st_mode & S_IXUSR)) /*usa stat para verificar si el archivo especificado en comando existe y si es ejecutable */
 		{
-			return (comando);
+			return (comando); /* si ya existe y es ejecutable rtorna comando (ruta completa) */
 		}
-		return (NULL);
+		return (NULL); /* si no existe o no es ejecutable retorna null */
 	}
 
-	path_copy = strdup(path);
-	if (path_copy == NULL)
+	path_copy = strdup(path); /*lo q hace es igualar path copy a una copia de path   */ 
+	if (path_copy == NULL) /* si path copy = null tira error duplicando path  */
 	{
 		perror("Error duplicando PATH");
 		return (NULL);
 	}
-	comando_ruta = malloc(1024);
-	if (comando_ruta == NULL)
+	comando_ruta = malloc(1024); /* reserva memoria para la variable comando ruta */
+	if (comando_ruta == NULL) /* si es igual a null, retorno error asignando memoria */
 	{
 		perror("Error asignando memoria");
-		free(path_copy);
-		return (NULL);
+		free(path_copy); /* y libero la memoria */
+		return (NULL); /*y como dio error retorna null  */
 	}
-	direct = strtok(path_copy, ":");
-	while (direct != NULL)
+	direct = strtok(path_copy, ":"); /*usa strtok para dividir la cadena path copy en tokens */ 
+	while (direct != NULL) /*el while recorre cada directorio de path obtenido por strtok */
 	{
-		if (ruta(direct, comando, comando_ruta, &buffer))
+		if (ruta(direct, comando, comando_ruta, &buffer)) /* llama a la funcion ruta pasando el directorio actual direct, el comando buscar comando, la variable comando_ruta,( para almacenar la ruta completa )y buffer para verificar la existencia del archivo */
 		{
-			free(path_copy);
-			return (comando_ruta);
+			free(path_copy);  /* si se encuentra la ruta del comando en uno de los directorios de path se libera la memoria de path copy */
+			return (comando_ruta); /* y se retorna comando_ruta */
 		}
-		direct = strtok(NULL, ":");
+		direct = strtok(NULL, ":"); /* continua al siguiente siguiente directorio de path */
 	}
-	free(comando_ruta);
-	free(path_copy);
-	return (NULL);
+	free(comando_ruta); /* si no se encuentra el comando en ningun directorio de path, se libera la memoria  */
+	free(path_copy); /* se libera path copy */
+	return (NULL); /* cunado se libera path copy se retorna null */
 }
 
 /**
@@ -79,8 +79,8 @@ char *buscar_path(char *comando, char *envp[])
  *Return: 1 si el comando se encuentra, 0 si no
  */
 
-int ruta(char *direct, char *comando, char *comando_ruta, struct stat *buffer)
+int ruta(char *direct, char *comando, char *comando_ruta, struct stat *buffer) /* direct es el directorio en el q se busca el comando ,comando es el nombre del comando a buscar, y comando_ruta es el espacio donde se almacena la ruta completa del comando y buffer es un struct de tipo stat para almacenar informacion del archivo */
 {
-	snprintf(comando_ruta, 1024, "%s/%s", direct, comando);
-	return (stat(comando_ruta, buffer) == 0 && (buffer->st_mode & S_IXUSR));
+	snprintf(comando_ruta, 1024, "%s/%s", direct, comando); /*costruye la ruta completa del comando conectando el directorio direct con el nombre del comando */
+	return (stat(comando_ruta, buffer) == 0 && (buffer->st_mode & S_IXUSR)); /* usa stat para verificar si el archivo existe en esa ruta, que es comando_ruta, y si es ejecutable */
 }
